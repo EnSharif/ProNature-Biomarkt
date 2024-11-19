ba9c3f02037b8ffe1ce74192bf75a7d8c0e62300
ENJI SHARIF <113913319+EnSharif@users.noreply.github.com>
2024-11-19 10:46:56 +0100

    add Project

---
+++ LoadingScreen.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProNatur_Biomarkt_GmbH
+{
+    public partial class LoadingScreen : Form
+    {
+        private int loadingBarValue;
+        public LoadingScreen()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadingScreen_Load(object sender, EventArgs e)
+        {
+            loadingBarTimer.Start();
+        }
+
+        private void loadingBarTimer_Tick(object sender, EventArgs e)
+        {
+            if (loadingBarValue <= 100)
+            {
+                loadingBarValue += 1;
+                lblLoadingProgress.Text = loadingBarValue.ToString() + " %";
+                loadingProgressBar.Value = loadingBarValue;
+
+            }
+            else
+            {
+                loadingBarTimer.Stop();
+            }
+            
+
+        }
+    }
+}
